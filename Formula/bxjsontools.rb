8d1007d06e31822128070d169ec9bdd025ba2998
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-05-15 14:52:11 +0200

    Add preliminary formula for BxJsontools

---
+++ Formula/bxjsontools.rb
@@ -0,0 +1,34 @@
+class BxJsontools < Formula
+  desc "C++ Class Library for JSON serialization"
+  homepage "https://github.com/BxCppDev/bxjsontools"
+
+  stable do
+    url "https://github.com/BxCppDev/bxjsontools",
+      :using => :git,
+      :tag => "0.1.0"
+    version "0.1.0"
+  end
+
+  devel do
+    url "https://github.com/BxCppDev/bxjsontools",
+      :using => :git
+  end
+
+  option :cxx11
+  option 'without-test', 'Disable test programs'
+
+  depends_on "cmake" => :build
+  # depends_on "boost"
+
+  def install
+    ENV.cxx11 if build.cxx11?
+    mkdir "brew-bxjsontools-build" do
+      args = std_cmake_args
+      args << "-DBXJSONTOOLS_ENABLE_TESTING=OFF" if build.with? "without-test"
+      system "cmake", "../", *args
+      system "make"
+      system "make", "test" if build.with? OS.linux?
+      system "install"
+    end
+  end
+end
