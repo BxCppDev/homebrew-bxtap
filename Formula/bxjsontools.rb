81622c79d41252ccc0168e1ef9d0e62bb1d9538d
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-05-15 16:43:49 +0200

    Fix bxjsontool formula

--- Formula/bxjsontools.rb
+++ Formula/bxjsontools.rb
@@ -1,25 +1,34 @@
 class Bxjsontools < Formula
-  desc "C++ Class Library for JSON serialization"
-  homepage "https://github.com/BxCppDev/bxjsontools"
-
-  head "https://github.com/BxCppDev/bxjsontools",
-      :branch => "master",
-      :tag => "0.1.0"
-
-  option 'without-test', 'Disable test programs'
+  desc ""
+  homepage ""
+  url "https://github.com/BxCppDev/bxjsontools/archive/0.1.0.tar.gz"
+  version "0.1.0"
+  sha256 "cc52b108182347b5599cf930d6af6f4dc25f51a2b89864cf7451485103851bf4"
 
   depends_on "cmake" => :build
-  # depends_on "boost"
 
   def install
-    ENV.cxx11
-    mkdir "brew-bxjsontools-build" do
-      args = std_cmake_args
-      args << "-DBXJSONTOOLS_ENABLE_TESTING=OFF" if build.with? "without-test"
-      system "cmake", "../", *args
-      system "make"
-      ### system "make", "test" if not build.with? "without-test"
-      system "install"
-    end
+   ENV.cxx11
+   # ENV.deparallelize
+   mkdir "brew-bxjsontools-build" do
+     args = std_cmake_args
+     args << "-DBXJSONTOOLS_ENABLE_TESTING=OFF" if build.with? "without-test"
+     system "cmake", "..", *args
+     system "make"
+     system "make", "install"
+   end
+  end
+
+  test do
+    # `test do` will create, run in and delete a temporary directory.
+    #
+    # This test will fail and we won't accept that! It's enough to just replace
+    # "false" with the main program this formula installs, but it'd be nice if you
+    # were more thorough. Run the test with `brew test bxjsontools`. Options passed
+    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
+    #
+    # The installed folder is not in the path, so use the entire path to any
+    # executables being tested: `system "#{bin}/program", "do", "something"`.
+    system "false"
   end
 end
