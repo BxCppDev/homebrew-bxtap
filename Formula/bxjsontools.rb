fa06803bee20ad0f05f6bb80d5fd23187a645ff8
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-05-16 11:24:53 +0200

    Fix formulae

--- Formula/bxjsontools.rb
+++ Formula/bxjsontools.rb
@@ -1,6 +1,6 @@
 class Bxjsontools < Formula
-  desc ""
-  homepage ""
+  desc "C++ JSON serialization API"
+  homepage "https://github.com/BxCppDev/bxjsontools"
   url "https://github.com/BxCppDev/bxjsontools/archive/0.1.0.tar.gz"
   version "0.1.0"
   sha256 "cc52b108182347b5599cf930d6af6f4dc25f51a2b89864cf7451485103851bf4"
@@ -8,14 +8,18 @@ class Bxjsontools < Formula
   needs :cxx11
   depends_on "cmake" => :build
 
+  option "without-test", "Inhibit test programs"
 
   def install
     ENV.cxx11
     mkdir "brew-bxjsontools-build" do
       args = std_cmake_args
-      args << "-DBXJSONTOOLS_ENABLE_TESTING=OFF" if build.with? "without-test"
+      args << "-DBXJSONTOOLS_ENABLE_TESTING=OFF" if build.without? "test"
       system "cmake", "..", *args
       system "make"
+      if build.with? "test"
+        system "make", "test"
+      end
       system "make", "install"
     end
   end
--- Formula/bxrabbitmq.rb
+++ Formula/bxrabbitmq.rb
@@ -1,15 +1,16 @@
 class Bxrabbitmq < Formula
   desc "C++ RabbitMQ client and server management API"
   homepage "https://github.com/BxCppDev/bxrabbitmq"
-  url "https://github.com/BxCppDev/bxrabbitmq/archive/0.1.0.tar.gz"
-  version "0.1.0"
-  sha256 "eada5771b6b2a6f558cc57e0f664c8578bd77523f4155e3032555f74f60433b2"
+  url "https://github.com/BxCppDev/bxrabbitmq/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "f975d86359c6550ca68b73c8954543aeafa147f3c3dbc6f396784bd8bcccd5dd"
 
   needs :cxx11
   depends_on "cmake" => :build
   depends_on "rabbitmq-c"
   depends_on "pkg-config"
 
+  option "without-test", "Inhibit test programs"
   option "with-manager", "Built the RabbitMQ server management API"
   if build.with? "manager"
     depends_on "bxjsontools" => [:optional, "with-manager"]
@@ -20,10 +21,14 @@ class Bxrabbitmq < Formula
     ENV.cxx11
     mkdir "brew-bxrabbitmq-build" do
       args = std_cmake_args
-      args << "-DBXRABBITMQ_WITH_MANAGER=ON" if build.with? "manager"
+      args << "-DBXRABBITMQ_ENABLE_TESTING=OFF"        if build.without? "test"
+      args << "-DBXRABBITMQ_WITH_MANAGER=ON"           if build.with? "manager"
       args << "-DBxJsontools_DIR=#{prefix}/lib/cmake/" if build.with? "manager"
       system "cmake", "..", *args
       system "make"
+      if build.with? "test"
+        system "make", "test"
+      end
       system "make", "install"
     end
   end
