53cc0ee1f02d20e3f1c9c7289561ec529cdbf4e1
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-05-17 11:11:25 +0200

    Fix formula for BxProtobuftools

--- Formula/bxprotobuftools.rb
+++ Formula/bxprotobuftools.rb
@@ -1,16 +1,16 @@
 class Bxprotobuftools < Formula
-  desc "C++ tools for Protocol Buffers"
+  desc "C++ tools for Protocol Buffers based serialization"
   homepage "https://github.com/BxCppDev/bxprotobuftools"
   url "https://github.com/BxCppDev/bxprotobuftools/archive/0.2.0.tar.gz"
   version "0.2.0"
   sha256 "47f4b4010a96876bae8673531ce85469848fc1dc0d9c05c8dc0d5bcced2ffd16"
   head "https://github.com/BxCppDev/bxprotobuftools.git", :branch => "develop"
 
+  option "without-test", "Inhibit test programs"
+
   needs :cxx11
   depends_on "cmake" => :build
   depends_on "bxcppdev/bxtap/protobuf@3.3.0"
-
-  option "without-test", "Inhibit test programs"
 
   def install
     ENV.cxx11
