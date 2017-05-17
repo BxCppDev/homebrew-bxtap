0476a5ad52fc631ef3743ffa40221533dc18fb0d
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-05-17 14:09:56 +0200

    Add/modify formulae

---
+++ Formula/camp.rb
@@ -0,0 +1,38 @@
+class Camp < Formula
+  desc "Tegesoft C++ reflection library"
+  homepage "https://github.com/tegesoft/camp"
+  url "https://github.com/tegesoft/camp.git",
+      :using => :git,
+      :tag => "0.8.0"
+  version "0.8.0"
+
+  option :cxx11
+
+  depends_on "cmake" => :build
+
+  # This appears to be a more robust way of defining options so that the
+  # dependency is always installed
+  option "with-doxygen", "Build with doxygen documentation"
+  depends_on "doxygen" => [:optional, :build]
+
+  if build.cxx11?
+    depends_on "bxcppdev/bxcppdev/boost" => "c++11"
+  else
+    depends_on "bxcppdev/bxcppdev/boost"
+  end
+
+  def install
+    system "touch", "LICENSE.LGPL3.txt"
+    mkdir "brew-camp-build" do
+      ENV.cxx11 if build.cxx11?
+      system "cmake", "..", *std_cmake_args
+      system "make"
+      system "make", "doc" if build.with? "doxygen"
+      system "make", "install"
+    end
+  end
+
+  test do
+    system "false"
+  end
+end
