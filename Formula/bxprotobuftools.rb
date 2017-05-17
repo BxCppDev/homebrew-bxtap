8f20fb80b09627259184f69aacce2a81c5994ef7
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-05-17 10:15:55 +0200

    Add formula for BxProtobuftools

---
+++ Formula/bxprotobuftools.rb
@@ -0,0 +1,32 @@
+class Bxprotobuftools < Formula
+  desc "C++ tools for Protocol Buffers"
+  homepage "https://github.com/BxCppDev/bxprotobuftools"
+  url "https://github.com/BxCppDev/bxprotobuftools/archive/0.2.0.tar.gz"
+  version "0.2.0"
+  sha256 "47f4b4010a96876bae8673531ce85469848fc1dc0d9c05c8dc0d5bcced2ffd16"
+
+  needs :cxx11
+  depends_on "cmake" => :build
+  depends_on "bxcppdev/bxtap/protobuf@3.3.0"
+
+  option "without-test", "Inhibit test programs"
+
+  def install
+    ENV.cxx11
+    mkdir "brew-bxprotobuftools-build" do
+      args = std_cmake_args
+      args << "-DBXPROTOBUFTOOLS_ENABLE_TESTING=OFF" if build.without? "test"
+      system "cmake", "..", *args
+      system "make"
+      if build.with? "test"
+        system "make", "test"
+      end
+      system "make", "install"
+    end
+  end
+
+  test do
+    system bin/"bxprotobuftools-query", "--help"
+  end
+
+end
