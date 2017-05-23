8d506f6ad158a79cd8f59e0a13cc3247cc8a0fab
Francois Mauger <mauger@lpccaen.in2p3.fr>
2017-05-23 02:02:20 +0200

    Update Bayeux formula

--- Formula/bayeux.rb
+++ Formula/bayeux.rb
@@ -16,7 +16,7 @@ class Bayeux < Formula
   end
 
   option "with-devtools", "Build debug tools for Bayeux developers"
-  option "without-test", "Inhibit test programs"
+  option "with-test",     "Build test programs"
 
   depends_on "cmake" => :build
   depends_on "doxygen" => :build
@@ -41,8 +41,9 @@ class Bayeux < Formula
       bx_cmake_args << "-DBAYEUX_COMPILER_ERROR_ON_WARNING=OFF"
       bx_cmake_args << "-DBAYEUX_WITH_QT_GUI=ON"
       bx_cmake_args << "-DBAYEUX_WITH_DEVELOPER_TOOLS=OFF" unless build.with? "devtools"
-      bx_cmake_args << "-DBAYEUX_ENABLE_TESTING=OFF" if build.without? "test"
+      bx_cmake_args << "-DBAYEUX_ENABLE_TESTING=ON" if build.with? "test"
       system "cmake", "..", *bx_cmake_args
+      system "make"
       if build.with? "test"
         system "make", "test"
       end
