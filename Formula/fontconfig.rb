993142a368ce6f85eb52ee610de91e5b13e97f4c
Francois Mauger <mauger@lpccaen.in2p3.fr>
2018-08-07 17:40:40 +0200

    Remove bottled rules for fontconfig formula

--- Formula/fontconfig.rb
+++ Formula/fontconfig.rb
@@ -4,30 +4,12 @@ class Fontconfig < Formula
   url "https://www.freedesktop.org/software/fontconfig/release/fontconfig-2.13.0.tar.bz2"
   sha256 "91dde8492155b7f34bb95079e79be92f1df353fcc682c19be90762fd3e12eeb9"
 
-  # The bottle tooling is too lenient and thinks fontconfig
-  # is relocatable, but it has hardcoded paths in the executables.
-  bottle do
-    sha256 "66ad096a51d6253be02b4d3df0299d422c8bd1bccf571ed64a8b8b21a2e77bc7" => :high_sierra
-    sha256 "d822bc26c6b556606087e5807293492814fedf9d408c857006b0886608010400" => :sierra
-    sha256 "a8016f2aff75677bab388a0ab138b4528d787c44c337f6ee7236e0de4a1cb268" => :el_capitan
-    sha256 "ffdc16db55b663c5f54779f7eb4f61ed59083fc6e67b7dec1fba0470b90f374b" => :x86_64_linux
-  end
-
-  pour_bottle? do
-    default_prefix = BottleSpecification::DEFAULT_PREFIX
-    reason "The bottle needs to be installed into #{default_prefix}."
-    # c.f. the identical hack in lua
-    # https://github.com/Homebrew/homebrew/issues/47173
-    satisfy { HOMEBREW_PREFIX.to_s == default_prefix }
-  end
-
-  head do
-    url "https://anongit.freedesktop.org/git/fontconfig", :using => :git
-
-    depends_on "autoconf" => :build
-    depends_on "automake" => :build
-    depends_on "libtool" => :build
-  end
+  # head do
+  #   url "https://anongit.freedesktop.org/git/fontconfig", :using => :git
+  #   depends_on "autoconf" => :build
+  #   depends_on "automake" => :build
+  #   depends_on "libtool" => :build
+  # end
 
   option "without-docs", "Skip building the fontconfig docs"
 
@@ -36,11 +18,12 @@ class Fontconfig < Formula
   unless OS.mac?
     depends_on "autoconf" => :build
     depends_on "automake" => :build
-    depends_on "gperf" => :build
-    depends_on "gettext" => :build
-    depends_on "libtool" => :build
+    depends_on "gperf"    => :build
+    depends_on "gettext"  => :build
+    depends_on "libtool"  => :build
     depends_on "bzip2"
-    depends_on "expat"
+    # Remove expat dependency and use system expat
+    ### depends_on "expat"
     # Breaks at install step on Ubuntu 18.04 :
     # 
     ### depends_on "util-linux" # for libuuid
